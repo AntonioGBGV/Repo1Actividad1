2d4a85d89d462f09fd2e72f95f8e4a8b7ae70825
Tonino74 <a.garijo.blanco@gmail.com>
2024-05-10 11:11:59 +0200

    el manager ha creado el proyecto base

---
+++ src/dao/CalculadoraDaoImpl.java
@@ -0,0 +1,35 @@
+package dao;
+
+public class CalculadoraDaoImpl implements CalculadoraDao{
+
+	@Override
+	public int sumar(int op1, int op2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int restar(int op1, int op2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int multiplicar(int op1, int op2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int dividir(int op1, int op2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int resto(int op1, int op2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

8a669b60030634746ea333204fb2bb1167a0ba9f
Tonino74 <a.garijo.blanco@gmail.com>
2024-05-10 11:46:43 +0200

    user1 ha implementado la logica de negocio a la clase calculadora dao

--- src/dao/CalculadoraDaoImpl.java
+++ src/dao/CalculadoraDaoImpl.java
@@ -5,31 +5,31 @@ public class CalculadoraDaoImpl implements CalculadoraDao{
 	@Override
 	public int sumar(int op1, int op2) {
 		// TODO Auto-generated method stub
-		return 0;
+		return op1 + op2;
 	}
 
 	@Override
 	public int restar(int op1, int op2) {
 		// TODO Auto-generated method stub
-		return 0;
+		return op1 - op2;
 	}
 
 	@Override
 	public int multiplicar(int op1, int op2) {
 		// TODO Auto-generated method stub
-		return 0;
+		return op1 * op2;
 	}
 
 	@Override
 	public int dividir(int op1, int op2) {
 		// TODO Auto-generated method stub
-		return 0;
+		return op1 / op2;
 	}
 
 	@Override
 	public int resto(int op1, int op2) {
 		// TODO Auto-generated method stub
-		return 0;
+		return op1 % op2;
 	}
 
 }
